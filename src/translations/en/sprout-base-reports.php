a88a7a99379051b65fef99c41971bf0f0b8e1db1
Ben Parizek <ben@barrelstrengthdesign.com>
2019-02-09 22:18:18 -0500

    Adds translations

---
+++ src/translations/en/sprout-base-reports.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * @link      https://sprout.barrelstrengthdesign.com/
+ * @copyright Copyright (c) Barrel Strength Design LLC
+ * @license   http://sprout.barrelstrengthdesign.com/license
+ */
+
+/**
+ * Sprout Base en Translation
+ *
+ * Returns an array with the string to be translated (as passed to `Craft::t()`) as
+ * the key, and the translation as the value.
+ *
+ * http://www.yiiframework.com/doc-2.0/guide-tutorial-i18n.html
+ */
+return [
+    'Delete' => 'Delete',
+    'Unable to find Report.' => 'Unable to find Report.',
+    'Name' => 'Name',
+    'Allow HTML <span class=\'info\'>Render HTML in your report results. Don\'t enable this setting if you allow user submitted content.</span>' => 'Allow HTML <span class=\'info\'>Render HTML in your report results. Don\'t enable this setting if you allow user submitted content.</span>',
+    'No results found' => 'No results found',
+    'View Report' => 'View Report',
+    'Auto-generate your report name using settings values, such as {startDate|date(\'F d, Y\')}' => 'Auto-generate your report name using settings values, such as {startDate|date(\'F d, Y\')}',
+    'Download CSV' => 'Download CSV',
+    'Couldn’t save report.' => 'Couldn’t save report.',
+    'Name Format' => 'Name Format',
+    'Could not find a report with id {reportId}' => 'Could not find a report with id {reportId}',
+    'No Report Group exists with id: {id}' => 'No Report Group exists with id: {id}',
+    'Select the \'New Report\' button in the top right to get started. Active Reports will be listed right here once they have been created.' => 'Select the \'New Report\' button in the top right to get started. Active Reports will be listed right here once they have been created.',
+    'Report deleted.' => 'Report deleted.',
+    'Save and continue editing' => 'Save and continue editing',
+    'Run Report' => 'Run Report',
+    'Handle' => 'Handle',
+    'Reports (Sprout)' => 'Reports (Sprout)',
+    'Submit Report' => 'Submit Report',
+    'Data Source not found.' => 'Data Source not found.',
+    'Sprout Reports' => 'Sprout Reports',
+    'Could not update report.' => 'Could not update report.',
+    'No report exists with the id “{id}”' => 'No report exists with the id “{id}”',
+    'Description' => 'Description',
+    'Save and add another report' => 'Save and add another report',
+    'Edit ' => 'Edit ',
+    'Unable to find the class: {type}. Confirm the appropriate Data Source integrations are installed.' => 'Unable to find the class: {type}. Confirm the appropriate Data Source integrations are installed.',
+    'Use a dynamic report name' => 'Use a dynamic report name',
+    'Group <span class=\'info\'>The group where this report should be displayed</span>' => 'Group <span class=\'info\'>The group where this report should be displayed</span>',
+    'Report group saved.' => 'Report group saved.',
+    'Export' => 'Export',
+    'Report saved.' => 'Report saved.',
+    'Download' => 'Download',
+    'Query updated.' => 'Query updated.',
+    'This report currently has no results. Check your report settings and ensure you have content for the report you are trying to run. To quickly edit the Report Settings, click on the gear icon in the upper right.' => 'This report currently has no results. Check your report settings and ensure you have content for the report you are trying to run. To quickly edit the Report Settings, click on the gear icon in the upper right.',
+    'Report not found.' => 'Report not found.',
+    'Are you sure you want to delete this report?' => 'Are you sure you want to delete this report?',
+    'Group deleted..' => 'Group deleted..',
+    'A description of your report that will appear in the info bubble next to your Report Name on the Reports Index page.' => 'A description of your report that will appear in the info bubble next to your Report Name on the Reports Index page.',
+    'Save Report' => 'Save Report',
+    'Report Group not found.' => 'Report Group not found.',
+    'Enabled' => 'Enabled',
+    'Reports' => 'Reports',
+    'Date Source not found.' => 'Date Source not found.',
+    'All reports' => 'All reports',
+    'New Report' => 'New Report',
+    'Data Source' => 'Data Source',
+    'What this report will be called in the CP.' => 'What this report will be called in the CP.',
+    'Create a custom report' => 'Create a custom report'
+];
