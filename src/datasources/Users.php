a6e07c404c80674b0b7d9f4538690929543df7ae
Ben Parizek <ben.parizek@gmail.com>
2019-12-07 21:19:35 -0500

    Update Users Data Source

--- src/datasources/Users.php
+++ src/datasources/Users.php
@@ -46,7 +46,7 @@ class Users extends DataSource
         }
 
         $userGroupIds = $settings['userGroups'] ?? false;
-        $displayUserGroupColumns = $settings['displayUserGroupColumns'] ?: false;
+        $displayUserGroupColumns = $settings['displayUserGroupColumns'] ?? false;
 
         $includeAdmins = false;
 
@@ -64,74 +64,71 @@ class Users extends DataSource
             $userGroupsByName[$userGroup->name] = 0;
         }
 
-        $query = (new Query())
-            ->select([
-                '[[users.id]]',
-                '[[users.username]] AS Username',
-                '[[users.email]] AS Email',
-                '[[users.firstName]] AS "First Name"',
-                '[[users.lastName]] AS "Last Name"'
-            ]);
+        $selectQueryString = "users.id,
+            users.username AS Username,
+            users.email AS Email,
+            (users.firstName) AS 'First Name',
+            (users.lastName) AS 'Last Name'";
 
         if ($displayUserGroupColumns) {
-            $query->addSelect([
-                '[[users.admin]] AS Admin'
-            ]);
+            $selectQueryString .= ',users.admin AS Admin';
         }
 
-        $query
-            ->from([Table::USERS . ' users'])
-            ->leftJoin(
-                Table::USERGROUPS_USERS . ' usergroups_users',
-                '[[users.id]] = [[usergroups_users.userId]]'
-            );
+        $query = new Query();
+        $userQuery = $query
+            ->select($selectQueryString)
+            ->from('{{%users}} users')
+            ->join('LEFT JOIN', '{{%usergroups_users}} usergroups_users', 'users.id = usergroups_users.userId');
 
         if (is_array($userGroupIds)) {
-            $query->where([
-                'in', '[[usergroups_users.groupId]]', $userGroupIds
-            ]);
+            $userQuery->where(['in', 'usergroups_users.groupId', $userGroupIds]);
         }
 
         if ($includeAdmins) {
-            $query->orWhere([
-                '[[users.admin]]' => true
-            ]);
+            $userQuery->orWhere('users.admin = 1');
         }
 
-        $query->groupBy('[[users.id]]');
-        $query->indexBy('id');
+        $userQuery->groupBy('users.id');
 
-        $users = $query->all();
+        // @todo - can we get users indexed by ID and simplify this?
+        $users = $userQuery->all();
 
-//        $query = new Query();
-//        $userGroupsMapQuery = $query
-//            ->select('*')
-//            ->from('{{%usergroups_users}} usergroups_users')
-//            ->join('LEFT JOIN', '{{%usergroups}} usergroups', 'usergroups.id = usergroups_users.groupId')
-//            ->all();
-//
-//        // Create a map of all users and which user groups they are in
-//        $userGroupsMap = [];
-//        foreach ($userGroupsMapQuery as $userGroupsUser) {
-//            $userGroupsMap[$userGroupsUser['userId']][$userGroupsUser['name']] = true;
-//        }
-//
-//        // Add and identify User Groups as columns
-//        foreach ($usersById as $key => $user) {
-//            if ($displayUserGroupColumns) {
-//                // Add User Groups as columns to user array
-//                $user = array_merge($user, $userGroupsByName);
-//
-//                if (isset($userGroupsMap[$key])) {
-//                    // Match users to the user groups they are in
-//                    $user = array_merge($user, $userGroupsMap[$key]);
-//                }
-//            }
-//
-//            $usersById[$key] = $user;
-//        }
+        // Update users to be indexed by their ids
+        $usersById = [];
+        foreach ($users as $user) {
+            $usersById[$user['id']] = $user;
+            unset ($usersById[$user['id']]['id']);
+        }
 
-        return $users;
+        $query = new Query();
+        $userGroupsMapQuery = $query
+            ->select('*')
+            ->from('{{%usergroups_users}} usergroups_users')
+            ->join('LEFT JOIN', '{{%usergroups}} usergroups', 'usergroups.id = usergroups_users.groupId')
+            ->all();
+
+        // Create a map of all users and which user groups they are in
+        $userGroupsMap = [];
+        foreach ($userGroupsMapQuery as $userGroupsUser) {
+            $userGroupsMap[$userGroupsUser['userId']][$userGroupsUser['name']] = true;
+        }
+
+        // Add and identify User Groups as columns
+        foreach ($usersById as $key => $user) {
+            if ($displayUserGroupColumns) {
+                // Add User Groups as columns to user array
+                $user = array_merge($user, $userGroupsByName);
+
+                if (isset($userGroupsMap[$key])) {
+                    // Match users to the user groups they are in
+                    $user = array_merge($user, $userGroupsMap[$key]);
+                }
+            }
+
+            $usersById[$key] = $user;
+        }
+
+        return $usersById;
     }
 
     /**
@@ -160,7 +157,7 @@ class Users extends DataSource
         $settingsErrors = $this->report->getErrors('settings');
         $settingsErrors = array_shift($settingsErrors);
 
-        return Craft::$app->getView()->renderTemplate('sprout-base-lists/_integrations/sproutreports/datasources/Users/settings', [
+        return Craft::$app->getView()->renderTemplate('sprout-base-reports/_components/datasources/Users/settings', [
             'userGroupSettings' => $userGroupSettings,
             'settings' => count($settings) ? $settings : $this->report->getSettings(),
             'errors' => $settingsErrors
