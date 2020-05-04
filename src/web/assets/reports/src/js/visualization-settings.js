20f58121e7f4664249ed8f4922639d46e6c9ec62
Ben Parizek <ben.parizek@gmail.com>
2020-05-03 21:14:40 -0400

    Update visualization settings js to js class

--- src/web/assets/reports/src/js/visualization-settings.js
+++ src/web/assets/reports/src/js/visualization-settings.js
@@ -1,88 +1,82 @@
 (function($) {
-  /** global: Craft */
-  /** global: Garnish */
-  var SproutVisualizationSettings = Garnish.Base.extend(
-    {
-      $toggle: null,
-      $target: null,
-      $addSeries: null,
 
-      init: function() {
-        this.$toggle = $('select[name="visualizationType"]');
-        this.$toggle.on("change", this.onVisualizationChange.bind(this));
-        $('.js-add-data-series').on("click", this.addDataSeries.bind(this));
-        $('table').on('click', '.js-remove-data-series', this.removeDataSeries.bind(this));
+  class SproutReportsVisualizationSettings {
 
-        this.findTarget();
-      },
+    constructor() {
+      this.$toggle = $('select[name="visualizationType"]');
+      this.$toggle.on("change", this.onVisualizationChange.bind(this));
+      $('.js-add-data-series').on("click", this.addDataSeries.bind(this));
+      $('table').on('click', '.js-remove-data-series', this.removeDataSeries.bind(this));
 
-      addDataSeries: function(event) {
-        event.preventDefault();
-        let $row = $(event.currentTarget).parent().find('[name*="dataColumn"]').last().closest('tr');
-        let $field = this.$target.find('[id$="visualizationDataColumn-field"]').first();
+      this.findTarget();
+    }
 
-        let $clone = $row.clone();
-        $clone.find('textarea').val('');
-        $row.after($clone);
+    addDataSeries(event) {
+      event.preventDefault();
+      let $row = $(event.currentTarget).parent().find('[name*="dataColumn"]').last().closest('tr');
 
-        return false;
-      },
+      this.$target.find('[id$="visualizationDataColumn-field"]').first();
 
-      removeDataSeries: function(event) {
-        event.preventDefault();
-        let $row = $(event.currentTarget).closest('tr');
+      let $clone = $row.clone();
+      $clone.find('textarea').val('');
+      $row.after($clone);
 
-        if ($row.siblings().length == 0) {
-          alert('You must defined at least one data column');
-        } else {
-          $row.remove();
-        }
-        return false;
+      return false;
+    }
 
-      },
+    removeDataSeries(event) {
+      event.preventDefault();
+      let $row = $(event.currentTarget).closest('tr');
 
-      onVisualizationChange: function() {
-        this.hideTarget(this.$target);
-        this.findTarget();
-        this.showTarget(this.$target);
-      },
-
-      findTarget: function() {
-        var targetSelector = this.$toggle.val();
-        if (targetSelector.length) {
-          targetSelector = '#' + this.getToggleVal();
-          this.$target = $(targetSelector);
-        } else {
-          this.$target = false;
-        }
-      },
-
-      hideTarget: function($target) {
-        if ($target && $target.length) {
-          $target.addClass('hidden');
-        }
-      },
-
-      showTarget: function($target) {
-
-        if ($target && $target.length) {
-          $target.removeClass('hidden');
-        }
-      },
-
-      getToggleVal: function() {
-        if (this.type === 'lightswitch') {
-          return this.$toggle.children('input').val();
-        } else {
-          var postVal = Garnish.getInputPostVal(this.$toggle);
-          return postVal === null ? null : postVal.replace(/[\[\]\\\/]+/g, '-');
-        }
+      if ($row.siblings().length === 0) {
+        alert('You must defined at least one data column');
+      } else {
+        $row.remove();
       }
-    });
+      return false;
 
+    }
 
-  Garnish.$doc.ready(function() {
-    Craft.SproutVisualizationSettings = new SproutVisualizationSettings();
-  });
+    onVisualizationChange() {
+      this.hideTarget(this.$target);
+      this.findTarget();
+      this.showTarget(this.$target);
+    }
+
+    findTarget() {
+      let targetSelector = this.$toggle.val();
+      if (targetSelector.length) {
+        targetSelector = '#' + this.getToggleVal();
+        this.$target = $(targetSelector);
+      } else {
+        this.$target = false;
+      }
+    }
+
+    hideTarget($target) {
+      if ($target && $target.length) {
+        $target.addClass('hidden');
+      }
+    }
+
+    showTarget($target) {
+
+      if ($target && $target.length) {
+        $target.removeClass('hidden');
+      }
+    }
+
+    getToggleVal() {
+      if (this.type === 'lightswitch') {
+        return this.$toggle.children('input').val();
+      } else {
+        const postVal = this.$toggle.find(':selected').val();
+        return postVal === null ? null : postVal.replace(/[\[\]\\\/]+/g, '-');
+      }
+    }
+  }
+
+  window.SproutReportsVisualizationSettings = SproutReportsVisualizationSettings;
+
 })(jQuery);
 
