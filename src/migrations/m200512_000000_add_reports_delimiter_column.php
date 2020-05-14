1a17c2f381366c394c0d68a110c810944f3f3490
Ben Parizek <ben.parizek@gmail.com>
2020-05-14 13:19:36 -0400

    Adds defaultExportDelimiter to base settings

--- src/migrations/m200512_000000_add_reports_delimiter_column.php
+++ src/migrations/m200512_000000_add_reports_delimiter_column.php
@@ -4,6 +4,7 @@ namespace barrelstrength\sproutbasereports\migrations;
 
 use barrelstrength\sproutbasereports\records\Report as ReportRecord;
 use craft\db\Migration;
+use craft\db\Query;
 use yii\base\NotSupportedException;
 
 class m200512_000000_add_reports_delimiter_column extends Migration
@@ -18,6 +19,24 @@ class m200512_000000_add_reports_delimiter_column extends Migration
         if (!$this->db->columnExists(ReportRecord::tableName(), 'delimiter')) {
             $this->addColumn(ReportRecord::tableName(), 'delimiter', $this->string()->after('sortColumn'));
         }
+
+        /** @noinspection ClassConstantCanBeUsedInspection */
+        $settingsColumn = (new Query())
+            ->select(['settings'])
+            ->from(['{{%sprout_settings}}'])
+            ->where(['model' => 'barrelstrength\sproutbasereports\models\Settings'])
+            ->scalar();
+
+        $settings = json_decode($settingsColumn, true);
+
+        // Add defaultExportDelimiter to shared settings
+        $settings['defaultExportDelimiter'] = ',';
+
+        /** @noinspection ClassConstantCanBeUsedInspection */
+        $this->update('{{%sprout_settings}}', [
+            'model' => 'barrelstrength\sproutbasereports\models\Settings',
+            'settings' => json_encode($settings)
+        ]);
 
         return true;
     }
