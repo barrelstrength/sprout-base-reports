8781c5335aa521ee8fd5ecec6ade30f9a70df1ca
Ben Parizek <ben.parizek@gmail.com>
2020-11-27 11:30:46 -0500

    Fixes migration error where legacy values for Users datasource may exist

--- src/migrations/m200520_000002_update_datasourceId_column_type.php
+++ src/migrations/m200520_000002_update_datasourceId_column_type.php
@@ -3,6 +3,7 @@
 namespace barrelstrength\sproutbasereports\migrations;
 
 use craft\db\Migration;
+use craft\db\Query;
 use yii\base\NotSupportedException;
 
 class m200520_000002_update_datasourceId_column_type extends Migration
@@ -17,6 +18,9 @@ class m200520_000002_update_datasourceId_column_type extends Migration
 
         // Updates foreign key if it does not exist. Try catch avoid errors if it exist
         if ($this->db->columnExists($reportTable, 'dataSourceId')) {
+
+            $this->cleanUpUsersDataSourceStuff();
+
             if ($this->db->getIsPgsql()) {
                 // Manually construct the SQL for Postgres`
                 // (see https://github.com/yiisoft/yii2/issues/12077)
@@ -27,6 +31,29 @@ class m200520_000002_update_datasourceId_column_type extends Migration
         }
 
         return true;
+    }
+
+    public function cleanUpUsersDataSourceStuff() {
+
+        $usersDataSourceId = (new Query())
+            ->select('id')
+            ->from('{{%sproutreports_datasources}}')
+            ->where([
+                'type' => 'barrelstrength\sproutbasereports\datasources\Users'
+            ])
+            ->scalar();
+
+        if ($usersDataSourceId) {
+            // update reports table with this ID
+            $this->update('{{%sproutreports_reports}}', [
+                'dataSourceId' => $usersDataSourceId
+            ], ['dataSourceId' => 'sproutreports.users'], [], false);
+        } else {
+            // remove all rows from reports table that match dataSourceId sproutreports.users
+            $this->delete('{{%sproutreports_reports}}', [
+                'dataSourceId' => 'sproutreports.users'
+            ]);
+        }
     }
 
     /**
